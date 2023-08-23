7aa2da0d36bb7663719c4a3e625770c80c61c9b2
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-08-23 23:52:09 +0530

    create counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react with Saurabh</h1>
+      <h1>Counter value: 5</h1>
+
+      <button>Add valute</button>
+      <br />
+      <button>remove value</button>
+    </>
+  )
+}
+
+export default App
