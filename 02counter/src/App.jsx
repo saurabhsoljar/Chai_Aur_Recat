bf50f616cd72b23da5f3a853f0a8adbbfa7e224b
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-08-27 11:03:18 +0530

    v8 interview qs

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,7 +10,12 @@ function App() {
   //let Counter = 15
 
   const addValue = () =>{
-    setCounter(counter + 1);
+    // setCounter(counter + 1);
+
+    setCounter(prevCounter => prevCounter+1);
+    setCounter(prevCounter => prevCounter+1);
+    setCounter(prevCounter => prevCounter+1);
+    setCounter(prevCounter => prevCounter+1);
     
   }
 
