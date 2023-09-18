95f0c8b2cf2b80992dca34477fb7a7bc8493535d
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-09-18 23:56:30 +0530

    Add local storage

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
 
@@ -13,10 +13,27 @@ function App() {
   const updatedTodo = (id,todo) =>{
     setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id
       === id ? todo : prevTodo)))
+
+      
   }
 
+  const deleteTodo = (id)=>{
+    setTodos((prev) => {prev.filter((todo) => todo.id !== id)})
+  }
+
+  const toggleComplete = (id) =>{
+    setTodos((prev) => prev.map((prevTodo) =>prevTodo === 
+    id ? {...prevTodo,completed: !prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    
+  }, [])
+  
+
   return (
-    <TodoProvider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
