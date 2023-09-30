148863b7e05fd47468c34a53f9ce13d692e3c2d0
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-09-30 20:58:58 +0530

    Add input and button

--- 12megaBlog/src/components/Header/Header.jsx
+++ 12megaBlog/src/components/Header/Header.jsx
@@ -15,7 +15,24 @@ function Header() {
             active: true
         },
         {
-
+            name: "Login",
+            slug: "/login",
+            active: !authStatus,
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus,
+        },
+        {
+            name: "All Posts",
+            slug: "/add-posts",
+            active: authStatus,
+        },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus,
         }
     ]
   return (
